64ef08b044fbe34730e73a389389c79cc4bd86fb
Leo <88931222+Leo4Bey@users.noreply.github.com>
2023-12-02 14:50:04 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çarpanları bulunacak sayıyı girer misin: ");
+            int sayi = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 1; i <= sayi; i++)
+            {
+                if(sayi%i ==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
